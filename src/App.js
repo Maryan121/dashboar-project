0443870646bd29b4987522c9e5bda0cd197f575d
Maryan121 <maryanmaxamuudsabriye@gmail.com>
2024-06-21 13:53:55 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import './App.css';
 import Customers from './components/customers';
-import CreateCustomer from './pages/createCustomer';
-import Suppliers from './pages/supplier';
-import CreateSupplier from './pages/createSupplier';
-// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 function App() {
  
   return (
     <div className='App'>
-      {/* <Router>
-        <Routes> 
-          <Route path='/'  element={<Customers />} />
-          <Route path="/supplier" element={<Suppliers />} />
-          <Route path="/createCustomer" element={<CreateCustomer />} />
-          <Route path="/createSupplier" element={<CreateSupplier />} />
-          <Route path="*" element={<div>404 page not found</div>} />
-        </Routes>
-      </Router> */}
       <Customers />
     </div>
 
--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ const MyRouter  = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <RouterProvider router={MyRouter} />
    </React.StrictMode>
 );
